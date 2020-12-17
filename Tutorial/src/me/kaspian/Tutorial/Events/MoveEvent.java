ef2fe5a6b7c1a9ff170018550614b8af66a5aa81
kacperleague9 <kacperleaguenine@gmail.com>
2020-12-17 20:20:00 +0100

    bye

--- Tutorial/src/me/kaspian/Tutorial/Events/MoveEvent.java
+++ Tutorial/src/me/kaspian/Tutorial/Events/MoveEvent.java
@@ -1,14 +1,27 @@
 package me.kaspian.Tutorial.Events;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class MoveEvent implements Listener {
 	
 	@EventHandler
-	public void onMove (PlayerMoveEvent event) {
-		System.out.println("UCIEKAJ TO GALI");
+	public void onMove (PlayerMoveEvent event) {	
+		
+		Player player = event.getPlayer();
+		Inventory pInver = player.getInventory();
+		ItemStack item = new ItemStack(Material.ANVIL, 1);
+		
+		if(!player.isOnGround()) {
+			pInver.addItem(item);
+			player.sendMessage(ChatColor.RED + "Ruszasz sie");
+		}
 	}
-
 }
